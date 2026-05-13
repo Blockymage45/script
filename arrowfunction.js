1daedec5f95708f1bc079e812b6a7af047537532
Adriano <150024041+Blockymage45@users.noreply.github.com>
2026-05-13 15:08:28 +0000

    fc gf xjf x

---
+++ arrowfunction.js
@@ -0,0 +1,20 @@
+function apresentar (nome){
+    return `meu nome é ${nome}`;
+}
+
+// Arrow Function
+const aprensentarArrow = nome => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2; 
+
+// Arrow function com + de 1 linha de instrução
+
+const somaNumeroPequenos = (num1, num2) => {
+if ( num1 > 10 || num2 > 10 ){
+    return "somente números de 1 a 9";
+}else{
+    return num1 + num2;
+ }
+}
+
+console.log(somaNumeroPequenos(4, 5));
+console.log(somaNumerosPequenos(15, 2));
